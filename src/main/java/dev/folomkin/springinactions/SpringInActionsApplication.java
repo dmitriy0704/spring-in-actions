2f54249061197e0eabb2ca9e3c4715738b8bd321
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-10-17 22:16:47 +0300

    init project

---
+++ src/main/java/dev/folomkin/springinactions/SpringInActionsApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.springinactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/folomkin/springinactions/SpringInActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.springinactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
